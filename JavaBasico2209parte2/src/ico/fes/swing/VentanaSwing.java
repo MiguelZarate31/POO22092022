3be377bf7bf2518bfde7978804301b9cc9e3684e
MiguelZarate31 <98933645+MiguelZarate31@users.noreply.github.com>
2022-05-12 22:36:54 -0500

    Add files via upload

---
+++ JavaBasico2209parte2/src/ico/fes/swing/VentanaSwing.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes.swing;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Migue
+ */
+public class VentanaSwing extends JFrame {
+    privateJtextfiel
+    
+    public VentanaSwing() throws Headless
+    
+}
